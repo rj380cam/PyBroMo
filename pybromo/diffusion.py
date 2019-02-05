dfd43d3e27def6265aec218e62b7321a40d74dfb
Antonino Ingargiola <tritemio@gmail.com>
2019-02-05 10:47:20 +0100

    Whitespaces

--- pybromo/diffusion.py
+++ pybromo/diffusion.py
@@ -1125,11 +1125,13 @@ class ParticlesSimulation(object):
         self.ts_store.h5file.flush()
         print('\n- End trajectories simulation - %s' % ctime(), flush=True)
 
+
 def sim_timetrace(emission, max_rate, t_step):
     """Draw random emitted photons from Poisson(emission_rates).
     """
     emission_rates = emission * max_rate * t_step
     return np.random.poisson(lam=emission_rates).astype(np.uint8)
+
 
 def sim_timetrace_bg(emission, max_rate, bg_rate, t_step, rs=None):
     """Draw random emitted photons from r.v. ~ Poisson(emission_rates).
