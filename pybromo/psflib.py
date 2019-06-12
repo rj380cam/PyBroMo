3d7e12f5b326bb651e049837a6b1bf1120019dcc
Antonino Ingargiola <tritemio@gmail.com>
2019-06-12 14:20:53 +0200

    cleanups

--- pybromo/psflib.py
+++ pybromo/psflib.py
@@ -85,15 +85,8 @@ class GaussianPSF:
         """
         xc, yc, zc = self.rc
         sx, sy, sz = self.s
-
-        ## Method1: direct evaluation
         return exp(-(((x-xc)**2)/(2*sx**2) + ((z-zc)**2)/(2*sz**2)))
-
-        ## Method2: evaluation using numexpr
-        #def arg(s):
-        #    return "((%s-%sc)**2)/(2*s%s**2)" % (s, s, s)
-        #return NE.evaluate("exp(-(%s + %s))" % (arg("x"), arg("z")))
-        
+       
     def hash(self):
         """Return an hash string computed on the PSF data."""
         return hashlib.md5(repr(self).encode()).hexdigest()
